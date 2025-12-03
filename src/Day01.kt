9fb96ea477de7ef0a5b3d47f5ff2e32a24714a86
Brandon Golley <bmgolley@gmail.com>
2025-12-02 20:30:31 -0600

    Day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val start = 50
+        val positions = input.runningFold(start) { acc, string ->
+            (string.replace('L', '-').replace('R', '+').toInt() + acc) % 100
+        }
+        return positions.count { it == 0 }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
