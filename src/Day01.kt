f83e965fdad17ba520aa81ddce08e58bf4452729
Brandon Golley <bmgolley@gmail.com>
2025-12-02 21:47:34 -0600

    Day 1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,3 +1,6 @@
+import kotlin.math.absoluteValue
+import kotlin.math.sign
+
 fun main() {
     fun part1(input: List<String>): Int {
         val start = 50
@@ -8,11 +11,34 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val start = 50
+        var count = 0
+        var position = start
+        for (rotationString in input) {
+            val prevPosition = position
+            val rotation = rotationString.replace('L', '-').replace('R', '+').toInt()
+            // count number of times rotated more than 100
+            count += (rotation / 100).absoluteValue
+            // add net change
+            position += rotation % 100
+            // and convert to actual position
+            position %= 100
+            if (position < 0) {
+                position += 100
+            }
+            // if rotation was negative but new position > previous position, or vice versa, then dial crossed 100
+            if (prevPosition != 0
+                && (position == 0 || prevPosition != position && (position - prevPosition).sign != rotation.sign)
+            ) {
+                count++
+            }
+        }
+        return count
     }
 
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
