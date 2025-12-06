b5d3798521389e62c993ea5c178f8fd1061df891
Brandon Golley <bmgolley@gmail.com>
2025-12-06 16:40:16 -0600

    Day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -7,6 +7,11 @@ const val ADJACENT_THRESHOLD = 4
 fun <E> List<E>.safeWindow(idx: Int) = subList(max(idx - 1, 0), min(idx + 1, lastIndex) + 1)
 fun String.safeWindow(idx: Int) = substring(max(idx - 1, 0), min(idx + 1, lastIndex) + 1)
 
+typealias Table<E> = List<MutableList<E>>
+
+operator fun <E> Table<E>.get(i: Int, j: Int) = this[i][j]
+operator fun <E> Table<E>.set(i: Int, j: Int, element: E) = this[i].set(j, element)
+
 fun main() {
     fun part1(input: List<String>): Int {
         return input.withIndex().sumOf { (i, row) ->
@@ -22,11 +27,34 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val table: Table<Char> = input.map(String::toMutableList)
+        var removed = 0
+        do {
+            val indexes = mutableListOf<Pair<Int, Int>>()
+            for ((i, row) in table.withIndex()) {
+                val safeWindow = table.safeWindow(i)
+                for ((j, cell) in row.withIndex()) {
+                    if (cell == ROLL) {
+                        val n = safeWindow.sumOf { rowI -> rowI.safeWindow(j).count { it == ROLL } } - 1
+                        if (n < ADJACENT_THRESHOLD) {
+                            indexes += i to j
+                        }
+                    }
+                }
+            }
+            val removedThisRound = indexes.size
+            for ((i, j) in indexes) {
+                assert(table[i, j] == '@')
+                table[i, j] = '.'
+            }
+            removed += removedThisRound
+        } while (removedThisRound > 0)
+        return removed
     }
 
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 13)
+    check(part2(testInput) == 43)
 
     val input = readInput("Day04")
     part1(input).println()
